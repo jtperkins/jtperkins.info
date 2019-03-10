5977daf7aac0ab1500a1af7d8e5282e2700b7a1c
JTPerkins <heershey@outlook.com>
2019-03-10 00:13:45 -0600

    clean navbar, footer and implemented scroll nav

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+$(document).ready(
+    function () {
+    if ( $(window).width() > 769) {   
+        var mywindow = $(window);
+        var mypos = mywindow.scrollTop();
+        var up = false;
+        var newscroll;
+        mywindow.scroll(function () {
+            newscroll = mywindow.scrollTop();
+            if (newscroll > mypos && !up) {
+                $('#nav').delay(1000).finish().slideUp(400);
+                up = !up;
+                console.log(up);
+            } else if(newscroll < mypos && up) {
+                $('#nav').finish().slideDown(300);
+                up = !up;
+            }
+            mypos = newscroll;
+        });
+        }})
